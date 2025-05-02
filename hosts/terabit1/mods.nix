aef11f55548d7f6469cde894039c218b22b445ae
herobrauni <braunnicolaj@gmail.com>
2025-05-02 15:02:31 +0200

    update

--- hosts/terabit1/mods.nix
+++ hosts/terabit1/mods.nix
@@ -1,7 +1,7 @@
 { lib, inputs, config, ... }: {
-  imports = [ ../../modules/k3s-agent.nix ];
-  sops.secrets.tskey-auth-k3s = { };
-  services.tailscale.authKeyFile = "/run/secrets/tskey-auth-k3s";
   # force disable tailscale
   # services.tailscale.enable = lib.mkForce false;
+  sops.secrets.tskey-auth-k3s = { };
+  services.tailscale.authKeyFile = "/run/secrets/tskey-auth-bf";
+  # imports = [ ../../modules/k3s-agent.nix ];
 }
