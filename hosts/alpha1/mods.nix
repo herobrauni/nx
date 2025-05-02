fb8c221a4ebad1a708e558269cb5b2cb27639e0b
herobrauni <braunnicolaj@gmail.com>
2025-05-02 19:54:37 +0200

    update

--- hosts/alpha1/mods.nix
+++ hosts/alpha1/mods.nix
@@ -1,7 +1,7 @@
 { lib, inputs, config, ... }: {
   # force disable tailscale
   # services.tailscale.enable = lib.mkForce false;
-  sops.secrets.tskey-auth-k3s = { };
+  sops.secrets.tskey-auth-bf = { };
   services.tailscale.authKeyFile = "/run/secrets/tskey-auth-bf";
   # imports = [ ../../modules/k3s-agent.nix ];
 }
