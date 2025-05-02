416f3c1e794eaf58276d4c615ac5e9426bd96045
herobrauni <braunnicolaj@gmail.com>
2025-05-02 20:38:33 +0200

    update

--- hosts/alpha1/mods.nix
+++ hosts/alpha1/mods.nix
@@ -4,4 +4,9 @@
   sops.secrets.tskey-auth-bf = { };
   services.tailscale.authKeyFile = "/run/secrets/tskey-auth-bf";
   # imports = [ ../../modules/k3s-agent.nix ];
+  modules = [
+    (nixpkgs.outPath + "/nixos/modules/profiles/minimal.nix")
+    (nixpkgs.outPath + "/nixos/modules/profiles/headless.nix")
+    (nixpkgs.outPath + "/nixos/modules/profiles/perlless.nix")
+  ];
 }
