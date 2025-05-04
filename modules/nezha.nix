61ca6735bf1f0889069931cceb722b002d5d6c01
herobrauni <braunnicolaj@gmail.com>
2025-05-04 19:45:58 +0200

    update

--- modules/nezha.nix
+++ modules/nezha.nix
@@ -5,8 +5,9 @@
   services.nezha-agent = {
     enable = true;
     clientSecretFile = "/run/secrets/nezha-client-secret";
-  };
+  
   settings = {
     server = "http://netcup1.brill-bebop.ts.net:8008";
+  };
   };
 }
