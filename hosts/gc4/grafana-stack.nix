84b0865c473667cfdff79c4bd2e45111b81d53b4
herobrauni <braunnicolaj@gmail.com>
2025-05-08 15:48:02 +0200

    update

--- hosts/gc4/grafana-stack.nix
+++ hosts/gc4/grafana-stack.nix
@@ -3,9 +3,44 @@
   # grafana configuration
   services.grafana = {
     enable = true;
-    domain = "grafana.brill-bebop.ts.net";
-    port = 2342;
-    addr = "127.0.0.1";
+    settings.server = {
+      domain = "gc4.brill-bebop.ts.net";
+      http_port = 2342;
+      http_addr = "127.0.0.1";
+    };
+    # domain = "grafana.brill-bebop.ts.net";
+    # port = 2342;
+    # addr = "127.0.0.1";
+    provision = {
+      dashboards.settings.providers = [
+        {
+          name = "Overview";
+          options.path = "/etc/grafana-dashboards";
+        }
+      ];
+
+      datasources.settings = {
+        apiVersion = 1;
+        datasources = [
+          {
+            name = "VictoriaMetrics";
+            type = "victoriametrics-metrics-datasource";
+            access = "proxy";
+            url = "http://127.0.0.1:8428";
+            isDefault = true;
+          }
+
+          {
+            name = "VictoriaLogs";
+            type = "victoriametrics-logs-datasource";
+            access = "proxy";
+            url = "http://127.0.0.1:9428";
+            isDefault = false;
+          }
+        ];
+      };
+    };
+
     declarativePlugins = with pkgs.grafanaPlugins; [
       victoriametrics-metrics-datasource
       victoriametrics-logs-datasource
--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -10,7 +10,6 @@
     enable = true;
     remoteWrite.url = "http://gc4.brill-bebop.ts.net:8428/api/v1/write";
     extraArgs = [ "-remoteWrite.label=instance=${config.networking.hostName}" ];
-
     prometheusConfig = {
       global = {
         external_labels = {
@@ -30,6 +29,59 @@
           scrape_interval = "10s";
           static_configs = [
             { targets = [ "127.0.0.1:8429" ]; }
+          ];
+        }
+      ];
+    };
+  };
+  services.promtail = {
+    enable = true;
+    configuration = {
+      server = {
+        http_listen_port = 0;
+        grpc_listen_port = 0;
+      };
+      positions.filename = "/tmp/positions.yaml";
+      clients = [
+        {
+          url = "http://gc4.brill-bebop.ts.net:9428/insert/loki/api/v1/push";
+        }
+      ];
+      scrape_configs = [
+        {
+          job_name = "journal";
+          journal = {
+            max_age = "6h";
+            labels = {
+              job = "systemd-journal";
+              host = config.networking.hostName;
+            };
+          };
+          relabel_configs = [
+            {
+              source_labels = [ "__journal_priority" ];
+              target_label = "priority";
+            }
+            {
+              source_labels = [ "__journal_priority_keyword" ];
+              target_label = "level";
+            }
+            {
+              source_labels = [ "__journal__systemd_unit" ];
+              target_label = "unit";
+            }
+            {
+              source_labels = [ "__journal__systemd_user_unit" ];
+              target_label = "user_unit";
+            }
+            {
+              source_labels = [ "__journal__boot_id" ];
+              target_label = "boot_id";
+            }
+            {
+              source_labels = [ "__journal__comm" ];
+              target_label = "command";
+            }
           ];
         }
       ];
