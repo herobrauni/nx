5dbe972a1441ad48c9064eb001a355791511239a
herobrauni <braunnicolaj@gmail.com>
2025-05-09 11:13:07 +0200

    update

--- modules/common.nix
+++ modules/common.nix
@@ -75,6 +75,23 @@
     # Use a non-standard port for better security
     ports = [ 666 ];
   };
+  networking.firewall = {
+    # enable the firewall
+    enable = true;
+
+    # always allow traffic from your Tailscale network
+    trustedInterfaces = [ "tailscale0" ];
+
+    # allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [
+      config.services.tailscale.port
+    ];
+
+    # allow you to SSH in over the public internet
+    allowedTCPPorts = [
+      666
+    ];
+  };
 
   # System maintenance
   nix = {
