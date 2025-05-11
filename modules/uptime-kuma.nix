22e00007b5fb387b0d5f5c9eb2b26027a06fcb77
herobrauni <braunnicolaj@gmail.com>
2025-05-11 20:27:08 +0200

    update

--- modules/uptime-kuma.nix
+++ modules/uptime-kuma.nix
@@ -1,4 +1,5 @@
 { config, lib, pkgs, ... }: {
+  systemd.tmpfiles.rules = [ "d /opt/uptime-kuma 0770 root root -" ];
   virtualisation = {
     podman = { enable = true; };
     oci-containers.containers = {
@@ -6,8 +7,15 @@
         image = "louislam/uptime-kuma:beta";
         extraOptions = [ "--pull=always" ];
         ports = [ "127.0.0.1:3001:3001" ];
-        volumes = [ "/opt/uptime-kuma:/app/data" ];
+        volumes = [ "uptime-kuma:/app/data" ];
       };
     };
+  };
+  sops.secrets.cloudflared-env = { };
+  services.cloudflared.enable = true;
+  services.cloudflared.tunnels."ca628217-fbdd-4414-9665-5cc7b0789bde" = {
+    credentialsFile = "/run/secrets/cloudflared-env";
+    default = "http_status:404";
+    ingress = { "kuma.brauni.dev" = "http://localhost:3001"; };
   };
 }
