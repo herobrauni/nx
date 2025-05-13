4d8d89d22154d32781b88a873e20ffcc455b8bf0
herobrauni <braunnicolaj@gmail.com>
2025-05-13 21:29:26 +0200

    update

--- modules/k3s-server.nix
+++ modules/k3s-server.nix
@@ -12,7 +12,16 @@
       --vpn-auth-file="/run/secrets/vpn-auth-file"
       --flannel-iface=tailscale0
       --node-external-ip=$''${hostIP config}
-      --resolv-conf=/etc/rancher/k3s/resolv.conf
+      --tls-san 'k.480p.com'
+      --secrets-encryption
+      --disable traefik
+      --disable local-storage
+      --disable metrics-server
+      --etcd-expose-metrics
+      --embedded-registry
+      --write-kubeconfig-mode 0644
+      --disable coredns
+      --disable-network-policy
     '';
     package = pkgs.k3s.overrideAttrs (oldAttrs: {
       installPhase =
