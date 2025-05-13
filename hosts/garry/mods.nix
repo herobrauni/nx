adaae99331148fc9e80454bdcc3f48d170ea9fce
herobrauni <braunnicolaj@gmail.com>
2025-05-14 00:10:44 +0200

    update

--- hosts/garry/mods.nix
+++ hosts/garry/mods.nix
@@ -3,5 +3,5 @@
   # services.tailscale.enable = lib.mkForce false;
   sops.secrets.tskey-auth-k3s = { };
   services.tailscale.authKeyFile = "/run/secrets/tskey-auth-k3s";
-  imports = [ ../../modules/k3s-server.nix ];
+  # imports = [ ../../modules/k3s-server.nix ];
 }
