c678eed9dffd38a84cab6a36603dc05548738871
herobrauni <braunnicolaj@gmail.com>
2025-05-14 02:49:26 +0200

    update

--- hosts/gc5/mods.nix
+++ hosts/gc5/mods.nix
@@ -1,7 +1,7 @@
 { lib, inputs, config, ... }: {
   # force disable tailscale
   # services.tailscale.enable = lib.mkForce false;
-  sops.secrets.tskey-auth-bf = { };
-  services.tailscale.authKeyFile = "/run/secrets/tskey-auth-bf";
+  sops.secrets.tskey-auth-k3s = { };
+  services.tailscale.authKeyFile = "/run/secrets/tskey-auth-k3s";
   # imports = [ ../../modules/k3s-agent.nix ];
 }
