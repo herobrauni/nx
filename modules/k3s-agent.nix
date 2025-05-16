85ffc05e4ce351c25ce9955a7830838ac1a4ed64
herobrauni <braunnicolaj@gmail.com>
2025-05-16 23:36:15 +0200

    update

--- modules/k3s-agent.nix
+++ modules/k3s-agent.nix
@@ -12,20 +12,20 @@
       --flannel-iface=tailscale0
       --node-external-ip=$''${hostIP config}
     '';
-    package =
-      let
-        patchedUtilLinux = pkgs.util-linuxMinimal.overrideAttrs (prev: {
-          patches = (prev.patches or []) ++ [ ./fix-mount-regression.patch ];
-        });
-        k3sWithPatchedUtilLinux = pkgs.k3s.override {
-          util-linux = patchedUtilLinux;
-        };
-      in
-        k3sWithPatchedUtilLinux.overrideAttrs (oldAttrs: {
-          installPhase =
-            lib.replaceStrings [ (lib.makeBinPath (oldAttrs.k3sRuntimeDeps)) ]
-            [ (lib.makeBinPath (oldAttrs.k3sRuntimeDeps ++ [ pkgs.tailscale ])) ]
-            oldAttrs.installPhase;
-        });
+    package = let
+      patchedUtilLinux = pkgs.util-linuxMinimal.overrideAttrs (prev: {
+        patches = (prev.patches or [ ]) ++ [ ./fix-mount-regression.patch ];
+      });
+      k3sWithPatchedUtilLinux =
+        pkgs.k3s.override { util-linux = patchedUtilLinux; };
+    in k3sWithPatchedUtilLinux.overrideAttrs (oldAttrs: {
+      installPhase =
+        lib.replaceStrings [ (lib.makeBinPath (oldAttrs.k3sRuntimeDeps)) ]
+        [ (lib.makeBinPath (oldAttrs.k3sRuntimeDeps ++ [ pkgs.tailscale ])) ]
+        oldAttrs.installPhase;
+    });
   };
+  networking.firewall.interfaces.tailscale0.allowedTCPPorts =
+    [ 6443 2379 2380 8472 10250 51820 51821 5001 ];
+  networking.firewall.trustedInterfaces = [ "tailscale0" ]; # for clustering
 }
--- modules/k3s-server.nix
+++ modules/k3s-server.nix
@@ -22,11 +22,20 @@
       "--disable coredns"
       "--disable-network-policy"
     ];
-    package = pkgs.k3s.overrideAttrs (oldAttrs: {
+    package = let
+      patchedUtilLinux = pkgs.util-linuxMinimal.overrideAttrs (prev: {
+        patches = (prev.patches or [ ]) ++ [ ./fix-mount-regression.patch ];
+      });
+      k3sWithPatchedUtilLinux =
+        pkgs.k3s.override { util-linux = patchedUtilLinux; };
+    in k3sWithPatchedUtilLinux.overrideAttrs (oldAttrs: {
       installPhase =
         lib.replaceStrings [ (lib.makeBinPath (oldAttrs.k3sRuntimeDeps)) ]
         [ (lib.makeBinPath (oldAttrs.k3sRuntimeDeps ++ [ pkgs.tailscale ])) ]
         oldAttrs.installPhase;
     });
   };
+  networking.firewall.interfaces.tailscale0.allowedTCPPorts =
+    [ 6443 2379 2380 8472 10250 51820 51821 5001 ];
+  networking.firewall.trustedInterfaces = [ "tailscale0" ]; # for clustering
 }
